37e7d134bc6564bd0956be3c0aa49c87d6356b3b
ezio84 <brabus84@gmail.com>
2020-01-07 00:38:43 +0100

    Update fingerprint to QQ1A.191205.011 6009058

--- abc.mk
+++ abc.mk
@@ -31,12 +31,12 @@ PRODUCT_MANUFACTURER := Google
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="bonito" \
     TARGET_DEVICE="bonito" \
-    PRIVATE_BUILD_DESC="bonito-user 10 QQ1A.191205.011 6009058 release-keys"
+    PRIVATE_BUILD_DESC="bonito-user 10 QQ1A.200105.002 6031801 release-keys"
 
-BUILD_FINGERPRINT := "google/bonito/bonito:10/QQ1A.191205.011/6009058:user/release-keys"
+BUILD_FINGERPRINT := "google/bonito/bonito:10/QQ1A.200105.002/6031801:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/bonito/bonito:10/QQ1A.191205.011/6009058:user/release-keys
+    ro.build.fingerprint=google/bonito/bonito:10/QQ1A.200105.002/6031801:user/release-keys
 
 BOARD_PREBUILT_VENDORIMAGE := vendor/images/bonito/vendor.img
 
